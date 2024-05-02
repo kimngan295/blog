6565d28646828db86ca47945b11e5b526c72e933
kimngan295 <138567605+kimngan295@users.noreply.github.com>
2024-05-02 12:18:06 +0700

    Delete src directory

--- src/middlewares/bodyParser.js
+++
@@ -1,7 +0,0 @@
-import bodyParser from 'body-parser';
-
-const { json, urlencoded } = bodyParser;
-export const bodyParserMiddleware = json();
-export const bodyParserMiddlewareUrlencoded = urlencoded({ extended: true });
-
-
--- src/utils/generatorJWT_SECRET_KEY.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-const generateSecretKey = () => {
-    return crypto.randomBytes(32).toString('hex');
-};
-
-const jwtSecretKey = generateSecretKey();
-console.log(jwtSecretKey)
